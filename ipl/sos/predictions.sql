bc93fdfd7b85206492f78bb383129c6af5b7a810
Christopher D. Long <galizur@gmail.com>
2021-09-18 02:55:45 -0400

    IPL update.

--- ipl/sos/predictions.sql
+++ ipl/sos/predictions.sql
@@ -8,7 +8,9 @@ sf2.team_id as away,
 (exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive)::numeric(5,1) as e_home,
 (exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive)::numeric(5,1) as e_away,
 ((exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive)-
-(exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive))::numeric(5,1) as e_d
+(exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive))::numeric(5,1) as e_d,
+20*(((exp(i.estimate)*sf1.offensive*o.exp_factor*sf2.defensive)-
+(exp(i.estimate)*sf2.offensive*d.exp_factor*sf1.defensive)))::numeric(5,1) as e_rd
 
 from ipl.games g
 join ipl._schedule_factors sf1
